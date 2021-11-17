69a4d9b67a137595f649947a281c4491640ca44b
Jeffrey W <66502522+JeffreyWangg@users.noreply.github.com>
2021-11-17 15:32:55 -0500

    better :))))

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 'use strict';
 
 var interval = 1000;
-var banners = document.querySelector("#banner-area");
-
+var i = 0;
+var banners = document.querySelectorAll(".banner div");
 function bannerMovement(){
         setInterval(() => {
-        console.log(banners);
+            if(i > 2){
+                i = 0;
+            }
 
+            for(var j = 0; j < 3; j++){
+                //i = currently displayed
+                //attach hide to all that isn't i
+                if(j == i){
+                    document.querySelectorAll(".banner")[j].classList.remove("hide");
+                } else {
+                    document.querySelectorAll(".banner")[j].classList.add("hide");
+                }
+            }
+            i++;
     }, interval);
 }
 
