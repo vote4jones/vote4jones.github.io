84f9677feeb71d415ed3cf6ab7aee940c2450242
Jeffrey W <66502522+JeffreyWangg@users.noreply.github.com>
2021-11-17 11:30:49 -0500

    updated website

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+var interval = 1000;
+var banners = document.querySelector("#banner-area");
+
+function bannerMovement(){
+        setInterval(() => {
+        console.log(banners);
+
+    }, interval);
+}
+
+bannerMovement();
